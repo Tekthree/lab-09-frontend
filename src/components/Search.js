4a0c175c94dc471d8e6e4c578f72eb20b1d2ae3f
Tek <tekthree.gmail.com>
2021-06-24 21:47:51 -0700

    signup finished, sign in almost

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,93 +1,210 @@
 import React, { Component } from "react";
-import { Card, Form, Col, Button, ListGroup } from "react-bootstrap";
+import { Card, Form, Col, Button, ListGroup, Container } from "react-bootstrap";
 import axios from "axios";
+import base64 from "base-64";
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      latlon: [],
-    
+      user: [],
+      signedIn: false,
+      signedUp: false,
+      shopping: true,
+      authorized: true,
+      name: "",
+      password: "",
+      roles: ""
     };
   }
 
+  
 
-
-  getLocation = async (event) => {
+  signIn = async (event) => {
     event.preventDefault();
-    const locationKey = process.env.REACT_APP_LOCATION_IQ_KEY;
-    try {
-      const backendParks = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${this.state.searchQuery}&format=json`;
-      const response = await axios.get(backendParks);
-      const location = response.data[0];
-      this.setState({ location: location });
-      this.getWeatherDay();
-    } catch (err) {
+    let base64encoded = base64.encode(`${this.state.name}:${this.state.password}`);
+
+    try{
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Authorization': 'Basic ' + base64encoded},
+        body: JSON.stringify(this.state.name , this.state.password)
+      };
+
+      const response = axios('https://lab401-09.herokuapp.com/signin', {requestOptions})
+
+      const userData = response.data;
+
+      this.setState({ user: userData })
+      console.log('this is the user response',this.state.user);
+
+    } catch(err){
+      console.log(err);
+    }
+   
+  };
+
+  signUp = async (event) => {
+    event.preventDefault();
+    try{
+      const body ={
+        "username": this.state.name,
+        "password": this.state.password,
+        "role": this.state.roles
+      }
+        axios({
+          method: 'POST',
+          url: 'https://lab401-09.herokuapp.com/signup',
+          data: body
+        })
+
+  
+        this.setState({ signedUp: true, signedIn: true })
+        console.log('this is the user response',this.state.user);
+
+    } catch(err){
       console.log(err);
     }
   };
 
 
   render() {
+    console.log(this.state.name, this.state.password, this.state.roles);
     return (
       <div>
-        <Card
-          text="light"
-          className="search-card"
-          style={{ marginTop: "100px" }}
-        >
-          <Card.Header>❤️🛒</Card.Header>
-          <Card.Body>
-            <Card.Title>Shoping list</Card.Title>
-            <Card.Text>
-              Log in to see shoping list
-            </Card.Text>
-            <Form>
-              <Form.Row>
-                <Col>
-                  <Form.Control placeholder="First name" />
-                </Col>
-                <Col>
-                  <Form.Control placeholder="Last name" />
-                </Col>
-              </Form.Row>
+        <Container>
+          {
+            (this.state.signedUp) === true ? (
+              <div></div>
+            ) : (
+            <Card
+            text="light"
+            className="search-card"
+            style={{ marginTop: "100px" }}
+          >
+            <Card.Header>❤️🛒</Card.Header>
+            <Card.Body>
+              <Card.Title>Shoping list</Card.Title>
+              <Card.Text>
+                Log in to see shoping list
+              </Card.Text>
+              <Form onSubmit={this.signUp}>
+                <Form.Row>
+                  <Col>
+                    <Form.Control 
+                    placeholder="Name"
+                    onChange={(e) => this.setState({ name: e.target.value })} 
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Control 
+                    placeholder="Password"
+                    onChange={(e) => this.setState({ password: e.target.value })} 
+                    />
+                  </Col>
+                </Form.Row>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>User</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>admin</option>
-                    <option>...</option>
-                  </Form.Control>
-                </Form.Group> 
-              </Form.Row>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>User Roles  </Form.Label>
+                    <Form.Control 
+                    as="select" 
+                    defaultValue="Choose..."
+                    onChange={(e) => this.setState({ roles: e.target.value })} 
+                    >
+                      <option value="admin">parent</option>
+                      <option value="user">child</option>
+                    </Form.Control>
+                  </Form.Group> 
+                </Form.Row>
+                <Button variant="primary" type="submit">
+                  Sign Up
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+            )
+          }
+          
+        </Container>
 
-        <div
-        style={{
-          marginTop: "30px",
-          paddingBottom: "10%",
-        }}
-        >
-        <Card>
-          <Card.Header>Shoping List</Card.Header>
-          <Card.Body>
-            <Card.Title></Card.Title>
-            <ListGroup variant="flush">
-              <ListGroup.Item>Cras justo odio</ListGroup.Item>
-              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            </ListGroup>
-            
-            <Button variant="primary">Display list</Button>
-          </Card.Body>
-        </Card>
-        </div>
+        <Container>
+          {
+            (this.state.signedIn) === false ? (
+              <div></div>
+            ) : (
+            <Card
+            text="light"
+            className="search-card"
+            style={{ marginTop: "100px" }}
+          >
+            <Card.Header>❤️🛒</Card.Header>
+            <Card.Body>
+              <Card.Title>Shoping list</Card.Title>
+              <Card.Text>
+                Log in to see shoping list
+              </Card.Text>
+              <Form onSubmit={this.signIn}>
+                <Form.Row>
+                  <Col>
+                    <Form.Control 
+                    placeholder="Name"
+                    onChange={(e) => this.setState({ name: e.target.value })} 
+                    value={this.state.name}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Control 
+                    placeholder="Pasword"
+                    onChange={(e) => this.setState({ password: e.target.value })} 
+                    value={this.state.password}
+                    />
+                  </Col>
+                </Form.Row>
+                <Button variant="primary" type="submit">
+                  Sign In
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+            )
+          }
+        </Container>
+
+        <Container>
+          {
+            (this.state.user) < 1 ? (
+              <div></div>
+            ) : (
+              <div
+              style={{
+                marginTop: "30px",
+                paddingBottom: "10%",
+              }}
+              >
+              <Card
+              className="shopping-card"
+              >
+                <Card.Header>Shoping List</Card.Header>
+                <Card.Body>
+                  <Card.Title></Card.Title>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>Cras justo odio <Button> Delete </Button></ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in <Button> Delete </Button></ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros <Button> Delete </Button></ListGroup.Item>
+                  </ListGroup>
+                  
+                  <Button variant="primary">Display list</Button>
+                </Card.Body>
+              </Card>
+              </div>
+            )
+          }
+        
+        </Container>
+        
+
+        
 
         
       </div>
